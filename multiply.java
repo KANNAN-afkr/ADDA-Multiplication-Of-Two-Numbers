5a6e2169a25bb9e0b550586f1f0473c8cc025b6c
KANNAN-afkr <squora150@gmail.com>
2026-02-19 12:38:39 +0530

    Multiplication Of Two Numbers

---
+++ multiply.java
@@ -0,0 +1,15 @@
+import java.util.*;
+class main {
+
+public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    System.out.print("Enter The First Number: ");
+    int a = sc.nextInt();
+    System.out.print("Enter The Second Number: ");
+    
+    int b = sc.nextInt();
+    int res = a*b;
+    System.out.print("The Result Of Multiplication Of "+a+" And "+b+" Is: "+res);
+}
+
+}
